907f05034b4a3dd99c533f0a2c68064e78bfbbfd
Naisargika Subudhi <106147044+Naisargika1402@users.noreply.github.com>
2023-05-13 10:57:19 +0530

    new

---
+++ script.js
@@ -0,0 +1,32 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+  shoppingCart.classList.remove('active');
+  loginSearch.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+  shoppingCart.classList.toggle('active');
+  searchForm.classList.remove('active');
+  loginSearch.classList.remove('active');
+}
+
+let loginSearch = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+  loginSearch.classList.toggle('active');
+  searchForm.classList.remove('active');
+  shoppingCart.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  loginForm.classList.remove('active');
+}
